c6d5156f9ffdf9cefdd835facd27ab305c24b5b9
Duran Martin <duranjeppe@gmail.com>
2020-02-11 13:47:01 +0200

    install script

--- install_bash.sh
+++ install_bash.sh
@@ -2,4 +2,4 @@
 apt update
 apt install -y vim git tmux docker.io neovim
 curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
-
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
